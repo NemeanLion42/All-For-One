6225318b557b20dfcf202d25ec9df7a6ab9e0c42
pocato3rd <rcato3rd@mit.edu>
2022-04-18 16:21:42 -0400

    adding specific room context requests plus a file reader

--- Assets/Scripts/Twitch Scripts/Commands/Context.cs
+++ Assets/Scripts/Twitch Scripts/Commands/Context.cs
@@ -7,13 +7,18 @@ public class Context : MonoBehaviour, IGameCommand
     public string CommandString => "context";
     public string ShortString => "c";
 
+    
 
-    float lastContextCmdReceived = -10.0f; // seconds
+    float lastContextCmdReceived = -15.0f; // seconds
     float minTimeBetweenSending = 10.0f; // seconds
 
     float lastMsgSent = -1.0f;
     float timeBetweenOptions = 2.0f; // seconds
 
+
+    // Handle the context table by reading in the file and updating contextHash
+    public TextAsset roomContextsFile;
+    private Hashtable contextHash = new Hashtable();
 
     List<string> contextMessagesToSend = new List<string>();
 
@@ -22,7 +27,22 @@ public class Context : MonoBehaviour, IGameCommand
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        // Read the text file containing room contexts
+        string roomContextContents = roomContextsFile.text;
+        string[] listOfRooms = roomContextContents.Split('\n');
+
+        foreach (string room_context in listOfRooms) {
+            // is the line a comment?
+            if (room_context.Substring(0, 2) != "//") {
+                // no! add it to our table
+                string[] name_msg_context = room_context.Split(':');
+                contextHash.Add(name_msg_context[0], name_msg_context[1]);
+            }
+            
+        }
+
+        Debug.Log(contextHash.Count);
     }
 
     // Update is called once per frame
@@ -31,6 +51,7 @@ public class Context : MonoBehaviour, IGameCommand
 
         if (Time.time > lastMsgSent + timeBetweenOptions && contextMessagesToSend.Count > 0) {
             string msg = contextMessagesToSend[0];
+
             _cm.SendMessageToChat(msg);
             contextMessagesToSend.RemoveAt(0);
 
@@ -40,7 +61,6 @@ public class Context : MonoBehaviour, IGameCommand
     }
 
     public bool Execute(string username, List<string> arguments, ChatManager cm = null) {
-
         if (cm != null) {
             _cm = cm;
         }
@@ -50,8 +70,27 @@ public class Context : MonoBehaviour, IGameCommand
             lastContextCmdReceived = Time.time;
 
             if (cm != null) {
-                contextMessagesToSend.Add("Charge port: Healing station");
-                contextMessagesToSend.Add("Weapons depot: Item store ");
+                bool we_have_args = (arguments.Count > 0) && (arguments[0] != "");
+
+                // are they asking about specific rooms?
+                if (we_have_args) {
+                    // yes! check each arg and send it if the room exists
+                    foreach (string arg in arguments) {
+
+                        // does the room exist in our context table?
+                        if (contextHash.ContainsKey(arg.ToLower())) {
+                            string msg_to_send = (string)contextHash[arg.ToLower()];
+                            contextMessagesToSend.Add(msg_to_send);
+                        }
+                    }
+
+                } else {
+                    // no! general check so just send all of the context messages
+                    foreach (string contextKeys in contextHash.Keys) {
+                        string msg_to_send = (string)contextHash[contextKeys];
+                        contextMessagesToSend.Add(msg_to_send);
+                    }
+                }
             }
             
         }
